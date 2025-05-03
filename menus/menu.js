d1b73fde7cdb452fa0984c15c358674eef574844
Sonhador Brasil <magazinegravatai@gmail.com>
2025-05-02 22:59:58 -0300

    Add files via upload

---
+++ menus/menu.js
@@ -0,0 +1,66 @@
+
+const menu = (prefix, nomeBot, numeroDono, nomeDono, hora, data, pushname, sender) => {
+return `
+*BY TOMIOKA</>, A.R.T🎩*
+╭─────────────────╮
+│ 📜 𝙈𝙀𝙉𝙐 𝘽𝙊𝙏 │
+╰─────────────────╯
+
+📌 *𝑳𝑰𝑺𝑻𝑨-𝑴𝑬𝑵𝑼:*  
+➜ ${prefix}menu  
+➜ ${prefix}menudono  
+➜ ${prefix}infodono  
+➜ ${prefix}menuadm  
+
+📌 *𝑴𝑬𝑵𝑼-𝑷𝑹𝑰𝑵𝑪𝑰𝑷𝑨𝑳:*  
+➜ ${prefix}ping  
+➜ ${prefix}infomestre  
+➜ ${prefix}pinterest  
+➜ ${prefix}playstore (roblox)  
+➜ ${prefix}metadinha  
+➜ ${prefix}nick (tomioka)  
+➜ ${prefix}imgpralink (img)  
+➜ ${prefix}videopralink (video)  
+➜ ${prefix}meme  
+
+📌 *𝑴𝑬𝑵𝑼-𝑩𝑹𝑰𝑵𝑪𝑨𝑫𝑬𝑰𝑹𝑨:*  
+➜ ${prefix}genio  
+➜ ${prefix}seujogo  
+➜ ${prefix}dia  
+➜ ${prefix}comunicar  
+➜ ${prefix}profissao  
+➜ ${prefix}gps  
+➜ ${prefix}sorte  
+
+📌 *𝑭𝑰𝑮𝑼𝑹𝑰𝑵𝑯𝑨𝑺:*  
+➜ ${prefix}attp  
+➜ ${prefix}attp2  
+➜ ${prefix}s  
+
+📌 *𝑫𝑶𝑾𝑳𝑶𝑨𝑫:*  
+➜ ${prefix}play (nome)  
+➜ ${prefix}play2 (nome)  
+➜ ${prefix}playvideo (nome)  
+➜ ${prefix}playmp3 (link)  
+➜ ${prefix}playmp4 (link)  
+
+📌 *𝑨𝑵𝑰𝑴𝑬𝑺:*  
+➜ ${prefix}hinata  
+➜ ${prefix}cosplay  
+➜ ${prefix}waifu2  
+➜ ${prefix}waifu  
+➜ ${prefix}yumeko  
+
+📌 *𝑼𝑺𝑼𝑨́𝑹𝑰𝑶:*  
+➜ ${prefix}novocmd  
+➜ ${prefix}bug  
+➜ ${prefix}servicos  
+➜ ${prefix}addsaldo  
+➜ ${prefix}saldo  
+
+━━━━━━━━━━━━━━━━━━  
+🌊 *𝘽𝙊𝙏 𝙊𝙉𝙇𝙄𝙉𝙀* 🌊  
+
+`
+}
+exports.menu = menu
